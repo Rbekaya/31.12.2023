1494e96d4e062879728060a4c3b8edde6f0d5cd0
Rbekaya <Rbekaya@gmail.com>
2024-01-01 14:18:13 +0200

    up

---
+++ menuOnLoad.js
@@ -0,0 +1,11 @@
+let id_menu,item_name,description,price,url
+
+window.onload = after_load
+
+function after_load(){
+    id = document.getElementById('id_dish')
+    dish = document.getElementById('dish_name')
+    des = document.getElementById('description')
+    price = document.getElementById('dish_price')
+    url = document.getElementById('dish_url')
+}
---
+++ menu_function.js
@@ -0,0 +1,37 @@
+//add click button 
+function add_click(){
+    let idv = parseInt(id_dish.value)
+    let dishv = dish_name.value
+    let desv = description.value
+    let pricev = parseInt(dish_price.value)
+    let urlv = dish_url.value
+     add_row(idv,dishv,desv,pricev,urlv)
+}
+
+function add_row(idv,dishv,desv,pricev,urlv){
+    let new_row = menu_table.insertRow(-1)
+
+    let cell_ID= new_row.inserCell(0)
+    let cell_Item_Name= new_row.inserCell(1)
+    let cell_Descriptio= new_row.inserCell(2)
+    let cell_Price= new_row.inserCell(3)
+    let cell_Picture_URL= new_row.inserCell(4)
+    
+    cell_ID.innerHTML = idv
+    cell_Item_Name.innerHTML = dishv
+    cell_Descriptio.innerHTML = desv
+    cell_Price.innerHTML = pricev
+    cell_Picture_URL.innerHTML = urlv
+    
+    clear_txt()
+}
+
+// clear data from inputs 
+function clear_txt(){
+    id_dish.value = ''
+    dish_name.value = ''
+    description.value = ''
+    dish_price.value = ''
+    dish_url.value = ''
+    id_dish.focus()
+}
